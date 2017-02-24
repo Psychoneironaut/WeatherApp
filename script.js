528b9d3724758163699624b63dcf504aafe5ac04
Psychoneironaut <willmabreyv@gmail.com>
2017-02-24 12:51:02 -0600

    Clean-up/geoFindMe() calls onLoad
    Removed "page loaded" alert, geoFindMe() is now called onLoad, and
    geoFindMe() no longer displays any info on the page.

--- script.js
+++ script.js
@@ -2,32 +2,23 @@ var latitude = 0;
 var longitude = 0;
 
 function geoFindMe() {
-  var output = document.getElementById("out");
+    var output = document.getElementById("out");
+    
+    // Do a few test cases where Geolocation isn't supported and make sure the error message looks good and cohesive with the rest of the page/content
+    if (!navigator.geolocation) {
+        output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+        return;
+    }
 
-  if (!navigator.geolocation){
-    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
-    return;
-  }
+    function success(position) {
+        latitude = position.coords.latitude;
+        longitude = position.coords.longitude;
+    }
 
-  function success(position) {
-    latitude  = position.coords.latitude;
-    longitude = position.coords.longitude;
-
-    output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
-
-    var img = new Image();
-    img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
-
-    output.appendChild(img);
-  }
-
-  function error() {
-    output.innerHTML = "Unable to retrieve your location";
-  }
-
-  output.innerHTML = "<p>Locating…</p>";
-
-  navigator.geolocation.getCurrentPosition(success, error);
+    function error() {
+        output.innerHTML = "Unable to retrieve your location";
+    }
+    navigator.geolocation.getCurrentPosition(success, error);
 }
 
 function getWeather() {
@@ -72,4 +63,4 @@ function getWeather() {
     });
 }
 
-window.onload = alert("page loaded");
+window.onload = geoFindMe();
